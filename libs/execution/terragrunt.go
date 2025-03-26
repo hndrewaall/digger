a636bfc55fe97adaa0badaa795fb0ff8a650dda4
Andrew Hall <hndrewaall@gmail.com>
2025-03-26 14:31:57 -0700

    Fix arg order

--- libs/execution/terragrunt.go
+++ libs/execution/terragrunt.go
@@ -73,8 +73,8 @@ func (terragrunt Terragrunt) Show(params []string, envs map[string]string, planA
 }
 
 func (terragrunt Terragrunt) runTerragruntCommand(command string, printOutputToStdout bool, envs map[string]string, arg ...string) (stdOut string, stdErr string, exitCode int, err error) {
-	args := []string{command}
-	args = append(args, "--experiment", "stacks", "stack", "run")
+	args := []string{}
+	args = append(args, "--experiment", "stacks", "stack", "run", command)
 	args = append(args, arg...)
 
 	expandedArgs := make([]string, 0)
